250f384dab9154a37d0fec3badb7a52f564612a9
arhank7009-eng <arhank7009@gmail.com>
2026-05-12 21:17:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import os
 # CONFIG
 # ==========================================
 
-BOT_TOKEN = "8408256623:AAFlV5mQKinX-rc75Hs6JjhKhCnZNt87fvA"
+BOT_TOKEN = "8408256623:AAHRV5RcyJfAPjNQgMDco7lw_qeJkDJ_gQY"
 API_URL = "https://adminpanels.shop/api/reseller_v1.php"
 
 API_KEY = "1c456265bf21eee11448ce37dcd469a3"
