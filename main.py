e50caee640a084d7878a0083a835a90a4fc758e2
arhank7009-eng <arhank7009@gmail.com>
2026-05-12 17:53:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,22 @@ import telebot
 from telebot import types
 import requests
 
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
 # ==========================================
 # CONFIG
 # ==========================================
