bfb48051fa24ab0d9c4d271b3fa3d545056bea32
arhank7009-eng <arhank7009@gmail.com>
2026-05-13 15:31:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -381,9 +381,9 @@ elif data[0] == "paid":
 
         return
 
-    bot.send_message(
-        call.message.chat.id,
-        f"""
+bot.send_message(
+    call.message.chat.id,
+    f"""
 ✅ PAYMENT SUCCESSFUL
 
 📦 PRODUCT:
